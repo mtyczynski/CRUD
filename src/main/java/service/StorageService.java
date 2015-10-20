e96530dc37f9de0bae6e3861e8b8abb1aa47a482
mtyczynski <mikit14@gmail.com>
2015-10-20 15:09:49 +0200

    dodanie lekarstw do storageservice

--- src/main/java/service/StorageService.java
+++ src/main/java/service/StorageService.java
@@ -1,5 +1,6 @@
 package service;
 
+import domain.Lekarstwo;
 import domain.Osoba;
 
 import java.util.ArrayList;
@@ -11,14 +12,21 @@ import java.util.List;
 
 public class StorageService {
     private List<Osoba> db = new ArrayList<Osoba>();
-
-    public void add(Osoba person){
+   private List<Lekarstwo> db2 = new ArrayList<Lekarstwo>();
+    public void addOsoba(Osoba person){
         Osoba newPerson = new Osoba(person.getImie(), person.getNazwisko());
         db.add(newPerson);
     }
 
+    public void addLekartswo(Lekarstwo lek){
+        Lekarstwo lekararstwo = new Lekarstwo(lek.getNazwa(),lek.getCena());
+        db2.add(lekararstwo);
+    }
     public List<Osoba> getAllPersons(){
         return db;
     }
 
+    public List<Lekarstwo> getDb2() {
+        return db2;
+    }
 }
